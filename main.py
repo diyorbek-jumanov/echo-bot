0913e22210f48a75e6868fe32a0a174afef21ec6
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-01 11:32:44 +0000

    return echo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 def sendMsg(ids, text):
     payload = {
@@ -9,27 +8,21 @@ def sendMsg(ids, text):
     token = '1623544581:AAGcmnpip3itggtxdUpX_GK7CdW-y1jmkic'
     url2 = f'https://api.telegram.org/bot{token}/sendMessage'
     r2 = requests.get(url2, params=payload)
-    # pprint(r2.json())
-
+    
 def echo():
     token = '1623544581:AAGcmnpip3itggtxdUpX_GK7CdW-y1jmkic'
     url1 = f'https://api.telegram.org/bot{token}/getUpdates'
     r1 = requests.get(url1)
-    # print(r1.json())
+    
     data = r1.json()
     updates = data['result']
 
-    # for update in updates:
-        # message = update['message']
-        # text = message['text']
-        # user = message['from']
-        # i = user['id']
-        # print(i)
     for update in updates:
         message = update['message']
         text = message['text']
         user = message['from']
         i = user['id']
+
         sendMsg(i, text)
 echo()
 
